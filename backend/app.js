1ab477a4d0dc6e2c7bbaacfafe4e56e14952e803
tanziqi <tanziqi.phoenix@bytedance.com>
2021-06-24 20:48:52 +0800

    加入RSA256加密，密钥长度至少1024.

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs').promises;  // file system
 
+// RSA生成公钥私钥
+// const NodeRSA = require('node-rsa');
+// const key = new NodeRSA({b: 1024});
+// const publicKey = key.exportKey('public');
+// const privateKey = key.exportKey('private');
+
+// fs.writeFile('./rsa/public.pem', publicKey).then((data) => {
+//     console.log('创建公钥成功');
+// }).catch((error) => {
+//     console.log('创建公钥失败');
+// });
+
+// fs.writeFile('./rsa/private.key', privateKey).then((data) => {
+//     console.log('创建私钥成功');
+// }).catch((error) => {
+//     console.log('创建私钥失败');
+// });
+
+
+// 路由控制
 const userRoutes = require('./routes/user');
 
 const app = express();
@@ -8,6 +29,7 @@ const app = express();
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
 
+// 跨域控制
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,3 +1,13 @@
+const fs = require('fs');
+const NodeRSA = require('node-rsa');
+const path = require('path');
+const rsaPath = path.resolve(__dirname, '../rsa').toString();
+const privateKey = fs.readFileSync(path.join(rsaPath, '/private.key'));
+const publicKey = fs.readFileSync(path.join(rsaPath, '/public.pem'));
+console.log('private key', privateKey);
+console.log('public key', publicKey);
+
+
 const _user = 'phoenix';
 const _password = '12345';
 
@@ -8,21 +18,22 @@ exports.login = (request, response, next) => {
   if ( user === _user && password === _password ) {
 
     const jwt = require('jsonwebtoken');
-    const token = jwt.sign({user: user}, 'mySecrete', 
-    // {expiresIn: '1min'}
-    );
+    const token = jwt.sign({user: user}, 
+      // 'mySecrete', 
+      privateKey,
+      {algorithm: 'RS256'}
+      );
 
     response.setHeader('Authorization', token);
     response.status(200).json({
       message: 'login succeeded'
     });
-    next();
+    return ;
   }
 
   response.status(401).json({
     message: 'login failed'
   });
-  next();
 };
 
 exports.records = (request, response) => {
@@ -48,16 +59,15 @@ exports.records = (request, response) => {
   console.log(token);
 
   const jwt = require('jsonwebtoken');
-  const decoded = jwt.verify(token, 'mySecrete');
+  const decoded = jwt.verify(token, publicKey);
   console.log(decoded);
 
   if ( decoded.user === _user ) {
     response.status(200).json({
       data: dataList
     });
-    next();
+    return ;
   }
-
 
   response.status(401).json({
     message: 'unauthorized'
